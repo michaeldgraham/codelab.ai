da2ffb11dee448ebeb31447fe3fd0a44390bc1bd
Webber Wang <webber@codelabstudios.com>
2020-09-20 18:18:35 -0700

    refactor(libs-core-node): add specs for node factory

--- libs/core/node/src/node-factory.spec.ts
+++ libs/core/node/src/node-factory.spec.ts
@@ -1,13 +1,52 @@
+import { NodeEntity } from './node-entity'
 import { NodeFactory } from './node-factory'
 import { NodeCreateDto } from '@codelab/shared/interface/node'
 
-describe('Node factory', () => {
-  it('creates a Node entity from create dto', () => {
-    const nodeDto: NodeCreateDto = {
-      type: 'React.Button',
-    }
-
+describe('Node Factory:', () => {
+  describe('Node Entity from a create DTO', () => {
+    let node: NodeEntity
     const factory = new NodeFactory()
-    const entity = factory.fromDto(nodeDto)
+
+    describe('with given values', () => {
+      const nodeDto: NodeCreateDto = {
+        id: 'node-id',
+        type: 'React.Button',
+        props: {
+          a: 'A',
+          b: 'B',
+        },
+        children: [
+          {
+            type: 'React.Text',
+          },
+        ],
+      }
+
+      beforeAll(() => {
+        node = factory.fromDto(nodeDto)
+      })
+
+      it('sets the ID', () => {
+        expect(node.id).toBe(nodeDto.id)
+        // expect(uuidValidate(node.id)).toBeTruthy()
+      })
+
+      it('sets the type', () => {
+        expect(node.type).toBe(nodeDto.type)
+      })
+
+      it('sets the props', () => {
+        expect(node.props).toBe(nodeDto.props)
+      })
+
+      // it('sets the children', () => {
+      //   expect(node.children).toMatchObject(nodeDto.children)
+      // })
+
+      // it('sets provides a default uuid v4 ID', () => {
+      //   expect(node).toHaveProperty('id')
+      //   expect(uuidValidate(node.id)).toBeTruthy()
+      // })
+    })
   })
 })
--- libs/core/node/src/node-factory.ts
+++ libs/core/node/src/node-factory.ts
@@ -1,9 +1,9 @@
 import { NodeEntity } from './node-entity'
-import { NodeCreateDto, NodeType } from '@codelab/shared/interface/node'
+import { NodeCreateDto, NodeTypeEnum } from '@codelab/shared/interface/node'
 import { Props } from '@codelab/shared/interface/props'
 
 export interface EntityFactory<T> {
-  fromDto<P extends Props = {}>(data: NodeCreateDto<NodeType, P>): T
+  fromDto<P extends Props = {}>(data: NodeCreateDto<NodeTypeEnum, P>): T
 }
 
 export class NodeFactory implements EntityFactory<NodeEntity> {
